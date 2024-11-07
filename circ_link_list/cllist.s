4fc99ebee5d4787b39b28df56db05f7fd4e0f19a
Gypra <gytisnaujasis@icloud.com>
2024-11-07 10:38:48 +0200

    Add del_node func

--- circ_link_list/cllist.s
+++ circ_link_list/cllist.s
@@ -6,6 +6,7 @@
         # Block size per node
         addi a2, a2, 12
         
+        # Load starting address of data at 0xA
         lui a6, %hi(START)
         # add a6, %lo(START)
         
@@ -22,6 +23,20 @@
         jal x1, alloc_node
         
         ##############
+        ##### V ######
+        ##############
+        
+        # Create next node
+        addi, t0, zero, 86
+        jal x1, alloc_node
+
+        # Add node to head
+        jal x1, add_tail
+        
+        # Update previous node
+        jal x1, update_node_before_tail
+        
+        ##############
         ##### I ######
         ##############
         
@@ -33,7 +48,7 @@
         jal x1, add_tail
         
         # Update previous node
-        jal x1, update_node
+        jal x1, update_node_before_tail
         
         ##############
         ##### S ######
@@ -46,7 +61,7 @@
         jal x1, add_tail
         
         # Update previous node
-        jal x1, update_node
+        jal x1, update_node_before_tail
         
         ##############
         ##### C ######
@@ -59,10 +74,20 @@
         jal x1, add_tail
         
         # Update previous node
-        jal x1, update_node
+        jal x1, update_node_before_tail
         
         # Print list
         jal x1, print_list
+        
+        ##############
+        # del node V #
+        ##############
+        lui a0, %hi(START)
+        add a1, a0, a2 # Get second node from head at a0
+        jal x1, del_node
+        
+        # Update nodes
+        jal x1, update_node_after_del
         
         jal x0, exit
     
@@ -90,15 +115,15 @@
         # Go back to main
         jalr x0, x1, 0
         
-    ###################
-    #### Add nodes ####
-    # node count = 2
-    # 12 byte blocks for each
-    # 24 bytes in total
-    # [value] 4 
-    # [next]  4
-    # [prev]  4
     add_tail:
+        ###################
+        #### Add nodes ####
+        # node count = 2
+        # 12 byte blocks for each
+        # 24 bytes in total
+        # [value] 4 
+        # [next]  4
+        # [prev]  4
         ##### Add node - a1 - to head node - a0 #####
         # Get -12
         li t5, -1
@@ -114,8 +139,43 @@
         
         # Go back to main
         jalr x0, x1, 0
+    
+    del_node:
+        sw zero, 0(a1)
+        sw zero, 4(a1)
+        sw zero, 8(a1)
         
-    update_node:
+        
+        jalr x0, x1, 0
+    
+    update_node_after_del:
+        # After deleting node at a1 (Node 2)
+        # Change previous node next at a1 - 12 (Node 1)
+        # And next node at a1 + 12 (Node 3)
+        # .next and .prev stored addresses
+        
+        # Get -12
+        li t5, -1
+        mul t6, a2, t5 # 12 * -1
+        
+        # Node 1
+        add t0, a1, t6
+        
+        # Node 3
+        add t1, a1, a2
+        
+        # Node_1.next -> Node3
+        sw t1, 4(t0)
+        
+        # Node_3.previous -> Node1
+        sw t0, 8(t1)
+        
+        jalr x0, x1, 0
+    
+    update_node_before_tail:
+        # After adding new tail
+        # Update previous node to point to tail (new node)
+        
         # Get -12
         li t5, -1
         mul t6, a2, t5 # 12 * -1
@@ -152,8 +212,6 @@
         ecall
         # Go back to main
         jalr x0, x1, 0
-    
-    del_node:
         
     exit:
         li a7, 10
