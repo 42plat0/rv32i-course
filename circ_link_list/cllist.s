536462d9b0ec7e92952b964e838309317271f4f6
Gypra <gytisnaujasis@icloud.com>
2024-11-12 14:04:54 +0200

    Add working solution without proper data storage in 9 bytes

--- circ_link_list/cllist.s
+++ circ_link_list/cllist.s
@@ -1,6 +1,5 @@
 .data
     START: .word 0
-
 .text
     main:
         # Block size per node
@@ -8,7 +7,7 @@
         
         # Load starting address of data at 0xA
         lui a6, %hi(START)
-        # add a6, %lo(START)
+        #addi a6, a6, %lo(START)
         
         addi a6, a6, -4
         
@@ -21,6 +20,71 @@
         # Head node
         addi, t0, zero, 82
         jal x1, alloc_node
+
+        ##############
+        ##### V ######
+        ##############
+        
+        # Create next node
+        addi, t0, zero, 86
+        jal x1, alloc_node
+        
+        # Add node to head
+        jal x1, add_tail
+
+
+        ##############
+        ##### I ######
+        ##############
+        # Create next node
+        addi, t0, zero, 73
+        jal x1, alloc_node
+        
+        # Add node to head
+        jal x1, add_tail
+
+        
+        ##############
+        ##### S ######
+        ##############
+        # Create next node
+        addi, t0, zero, 83
+        jal x1, alloc_node
+        
+        # Add node to head
+        jal x1, add_tail
+
+
+        ##############
+        ##### C ######
+        ##############
+        # Create next node
+        addi, t0, zero, 67
+        jal x1, alloc_node
+
+        # Add node to head
+        jal x1, add_tail
+        
+        # Print list
+        jal x1, print_list
+        
+        ##################
+        ##### del V ######
+        ##################
+
+        # Load head at 0xA because print_list overwrote it
+        lui a6, %hi(START)
+        
+        # Save head node address
+        add a0, a6, zero
+        
+        # Choose node to delete and store to a1
+        addi a1, a0, 12
+        
+        # Save addresses of .next & .prev of to be deleted node 
+        # And delete it
+        jal x1, del_node
+
         
         ##############
         ##### V ######
@@ -33,106 +97,28 @@
         # Add node to head
         jal x1, add_tail
         
-        # Update previous node
-        jal x1, update_node_before_tail
-        
-        ##############
-        ##### I ######
-        ##############
-        
-        # Create next node
-        addi, t0, zero, 73
-        jal x1, alloc_node
-
-        # Add node to head
-        jal x1, add_tail
-        
-        # Update previous node
-        jal x1, update_node_before_tail
-        
-        ##############
-        ##### S ######
-        ##############
-        # Create next node
-        addi, t0, zero, 83
-        jal x1, alloc_node
-
-        # Add node to head
-        jal x1, add_tail
-        
-        # Update previous node
-        jal x1, update_node_before_tail
-        
-        ##############
-        ##### C ######
-        ##############
-        # Create next node
-        addi, t0, zero, 67
-        jal x1, alloc_node
-
-        # Add node to head
-        jal x1, add_tail
-        
-        # Update previous node
-        jal x1, update_node_before_tail
-        
-        # Print list
+        # Print new list
         jal x1, print_list
         
-        ##############
-        # del node V #
-        ##############
-        lui a0, %hi(START)
-        add a1, a0, a2 # Get second node from head at a0
-        jal x1, del_node
-        
-        # Update nodes
-        jal x1, update_node_after_del
-        
-        # Iterpti i istrinto node'o vieta kaip taila ir sulinkinti tinkamai
-        # Pvz. N1 -> N2 -> N3 -> N4
-        # Pvz. N1 -> *deleted* -> N3 -> N4
-        # Tai. N1 -> *N5* -> N3 -> N4
-        # Tai. N1 -> N5 -> N3 -> N4 -> *N6*
-        ######################################
-        # Problema su tailo addinimu
-        ##############
-        ##### C ######
-        ##############
-        
-        # Load starting address of data at 0xA
-        lui a6, %hi(START)
-        # add a6, %lo(START)
-        
-        addi a6, a6, -4
-        
-        # Create next node
-        addi, t0, zero, 63
-        jal x1, alloc_node
-        
-        # Add node to head
-        jal x1, add_tail
-        
-        # Update previous node
-        jal x1, update_node_before_tail
-        
         # Exit
-        jal x0, exit
-    
-    alloc_node:
-        # Ieskoti tuscios vietos kuri prasideda su 0
+        jal x1, exit
         
+    alloc_node:        
         # Go to next memory segment
         addi a6, a6, 4
         
         # Load current mem address value
         lw t1, 0(a6)
         
+        # Ieskoti tuscios vietos kuri prasideda su 0
         # Checks if current memory block is empty
         bnez t1, alloc_node
         
-        # Save current address of node
-        add a1, a6, zero 
+        # Save address of newly created node
+        add s0, a6, zero
+        
+        # Load newly created node address
+        add a1, s0, zero
         
         # Save value
         sb t0, 0(a6)
@@ -141,16 +127,14 @@
         addi a6, a6, 4
 
         # Save NEXT
-        sw a6, 0(a6)
-        # Get next node address
-        # addi t6, a6, 8
-
+        sw a1, 0(a6)
+        
         # Go to next memory segment
         addi a6, a6, 4
-        # Save PREV
-        sw a6, 0(a6)
         
-        # Issaugoti naujo NODe addressa i S0
+        # Save PREV
+        sw a1, 0(a6)
+        
         
         # Go back to main
         jalr x0, x1, 0
@@ -165,94 +149,96 @@
         # [next]  4
         # [prev]  4
         ##### Add node - a1 - to head node - a0 #####
-        # Get -12
-        li t5, -1
-        mul t6, a2, t5 # 12 * -1
+
+        # LOAD PREVIOUS TAIL AND HEAD
+        # load tail at head's previous
+        lw t1, 8(a0) # t1 = head.previous
+        # load head at tail's previous
+        lw t2, 4(t1) # t2 = tail.next
         
+        # UPDATE NEW_NODE'S PREV AND NEXT
         # new_node.next -> head
-        sw a0, 4(a1)
-	    # new_node.prev -> previous_node (perrasys apatine eilute!)
-        add t0, a1, t6 # Previous node
-        sw t0, 8(a1)
+        sw t2, 4(a1)
+	    # new_node.prev -> previous_tail_node 
+        sw t1, 8(a1)
+        
+        # UPDATE HEAD'S PREV TO NEW_NODE
         # head_node.prev -> tail
         sw a1, 8(a0)
         
-        # Go back to main
+        # UPDATE BEFORE-TAIL.NEXT AND HEAD.PREVIOUS
+        # Save before-tail.next -> new_node
+        sw a1, 4(t1)
+        # Save head.previous -> new_node
+        sw a1, 8(t2)
+        
+        # Return
         jalr x0, x1, 0
     
     del_node:
+        # save previous node: 	previous_node = `del_node.previous`
+        # s1 = deleted_node.previous
+        # save next node:     	next_node = `del_node.next`
+        # s2 = deleted_node.next
+        
+        # SAVE NODE ADDRESSES
+
+        # Load deleted_node.previous
+        lw s1, 4(a1)
+        # Load deleted_node.next
+        lw s2, 8(a1)
+
+        # DELETE NODE
         sw zero, 0(a1)
         sw zero, 4(a1)
         sw zero, 8(a1)
         
+        # UPDATE NODES
+        # Update next node's previous to s2
+        addi t1, s1, 8
+        sw s2, 0(t1)
         
-        jalr x0, x1, 0
-    
-    update_node_after_del:
-        # After deleting node at a1 (Node 2)
-        # Change previous node next at a1 - 12 (Node 1)
-        # And next node at a1 + 12 (Node 3)
-        # .next and .prev stored addresses
+        # Update prev node's next to s1
+        addi t1, s2, 4
+        sw s1, 0(t1)
         
-        # Get -12
-        li t5, -1
-        mul t6, a2, t5 # 12 * -1
-        
-        # Node 1
-        add t0, a1, t6
-        
-        # Node 3
-        add t1, a1, a2
-        
-        # Node_1.next -> Node3
-        sw t1, 4(t0)
-        
-        # Node_3.previous -> Node1
-        sw t0, 8(t1)
-        
-        jalr x0, x1, 0
-    
-    update_node_before_tail:
-        # After adding new tail
-        # Update previous node to point to tail (new node)
-        
-        # Get -12
-        li t5, -1
-        mul t6, a2, t5 # 12 * -1
-        
-        # update previous node 
-        # previous_node.next -> tail
-        add t0, a1, t6
-        sw a1, 4(t0)
-        
-        # Go back to main
+        # Return
         jalr x0, x1, 0
         
     print_list:
-        add t0, a0, zero
+        # PRINT HEAD NODE AND TRAVERSE THE LIST        
+        # save head address
+        add s3, a0, zero
         
-        lw a0, 0(t0)
+        # print head's value
+        lw a0, 0(s3)
         li a7, 11
         ecall
         
-        lw a0, 12(t0)
-        li a7, 11
-        ecall
-
-        lw a0, 24(t0)
-        li a7, 11
-        ecall
-
-        lw a0, 36(t0)
-        li a7, 11
-        ecall
-
-        lw a0, 48(t0)
-        li a7, 11
-        ecall
+        # save head.next
+        addi t1, s3, 4
+        
+        traverse_list:
+            # Value address of node
+            lw t2, 0(t1)
+            
+            lw a0, 0(t2)
+            li a7, 11
+            ecall 
+            
+            # Get .next value of current node
+            addi t1, t2, 4
+            
+            # Check what next address is
+            lw t3, 0(t1)
+            
+            # Loop again, if head node hasn't been reached
+            bne t3, s3, traverse_list
+        
+        
         # Go back to main
         jalr x0, x1, 0
-        
+    
     exit:
         li a7, 10
         ecall
