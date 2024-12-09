2396bf87ca003e05a7f0b6a457ccc5695337d2b2
42plat0 <gytisnaujasis@icloud.com>
2024-12-09 16:24:38 +0200

    Refactor code

--- read_write/read_file.s
+++ read_write/read_file.s
@@ -30,7 +30,7 @@ error_msg:
 .globl _start
 
 _start:
-    addi sp, sp, -1024
+    # addi sp, sp, -1024 # Create stack for results
 
     lui s0, %hi(bufferstr)
     addi s0, s0,%lo(bufferstr) # Set buffer address
@@ -64,134 +64,152 @@ read_loop:
     ecall
     bltz a0, write_error       # Exit if write failed
 
-    jal x1, count_sentences
-    jal x1, count_words
-    jal x1, count_cases
+
+    ### Counting and saving ###
+
+    ## Cases ##
+    jal x1, count_cases     # get U/L case count, t2 = UpC, t3=LoC
+    
+    mv a0, t3               # Use L case count as arg
+    jal x1, save_count      # Count L case count
+    
+    mv a0, t2               # Use U case count as arg
+    jal x1, save_count      # Count U case count
+    
+    ## Words ##
+    jal x1, count_words     # Get word count
+    
+    mv a0, t2               # Use word count as arg
+    jal x1, save_count      # Save word count
+
+    ## Sentences ##
+    jal x1, count_sentences # get sentence count
+    
+    mv a0, t2               # Use sentence count as arg
+    jal x1, save_count      # Save sentence count
+
+
+    ### Prints ###
+
+    # Sentence
+    la a1, sentence_msg     # Sentence message address
+    li a2, 12               # Message length
+    jal x1, print_msg
+    jal x1, print_result
+    
+    # Word
+    la a1, word_msg         # Word message address
+    li a2, 8                # Message length
+    jal x1, print_msg 
+    jal x1, print_result
+
+    # Cases
+    la a1, ucase_msg        # Uppercase message address
+    li a2, 12               # Message length
+    jal x1, print_msg
+    jal x1, print_result
+    
+    la a1, lcase_msg        # Lowercase message address
+    li a2, 12               # Message length
+    jal x1, print_msg
+    jal x1, print_result
+
 
     j close_file                # Continue reading
 
+print_msg: # print string before count
+
+    # Write to stdout
+    li a0, 1                   # Stdout file descriptor
+    li a7, 64                  # Syscall number for write
+    ecall
+    jalr x0, x1, 0
+
+print_result:
+    li a0, 1                   # Stdout file descriptor
+    mv a1, sp                  # Buffer address
+    li a2, 3                   # Len 
+    li a7, 64                  # Syscall number for write
+    ecall
+
+    # Write to stdout
+    li a0, 1                   # Stdout file descriptor
+    la a1, newline             # Message address
+    li a2, 1                   # Message length
+    li a7, 64                  # Syscall number for write
+    ecall
+
+    addi sp, sp, 3 # Increment sp to print next number 
+
+    jalr x0, x1, 0
+
+save_count:
+    # a0 = count, t6 = divisor, t3 = hundreds, t1 = tens, t5 = ones
+    # Get 100s 10ths and 1s
+    li t6, 100
+    div t3, a0, t6 # Hundreds count
+    rem t4, a0, t6 # 10's
+
+    li t6, 10
+    div t1, t4, t6 # 10's count
+    rem t5, t4, t6 # 1's count
+    
+    # Get num ascii code
+    addi t3, t3, 48 # Hundreds
+    addi t1, t1, 48 # Tens
+    addi t5, t5, 48 # Ones
+
+    # Add numbers to stack
+
+    # Ones
+    addi sp, sp, -1 
+    sb t5, 0(sp)
+    
+    # Tens
+    addi sp, sp, -1
+    sb t1, 0(sp)
+
+    # Hundreds
+    addi sp, sp, -1
+    sb t3, 0(sp)
+
+    jalr x0, x1, 0
+
 count_cases:
-    addi t2, zero, -1 # Uppercase ctr
-    add t3, zero, zero # Lowercase ctr
-    mv t1, s0
+    mv t1, s0                       # Buffer
+    addi t2, zero, -1               # Uppercase ctr
+    add t3, zero, zero              # Lowercase ctr
 
     add_ucase_count:
-        addi t2, t2, 1
-        beq zero, zero, loop_cases
+        addi t2, t2, 1              # UpC counter
+        beq zero, zero, loop_cases  # Go back to loop
     
     add_lcase_count:
-        addi t3, t3, 1
+        addi t3, t3, 1              # LoC counter
 
-    loop_cases:
-        # check for cases
-        lb t4, 0(t1)
+    loop_cases: # check for cases
+        lb t4, 0(t1)                # Load letter
         
-        beqz t4, print_cases
+        beqz t4, go_back 
 
-        addi t1, t1, 1 # go to next letter
+        addi t1, t1, 1              # go to next letter
         
-        addi t0, t4, -65 # subtract first Uppercase letter ascii code
-        blt t0, zero, loop_cases # Is not a letter
-        beqz t0, add_ucase_count # Check Upper case start
-        addi t5, zero, 25 # Upper case end
-        ble t0, t5, add_ucase_count # Is not a letter
+        addi t0, t4, -65            # subtract first Uppercase letter ascii code
+        blt t0, zero, loop_cases    # Is not a letter
+        beqz t0, add_ucase_count    # Check Upper case start
+        addi t5, zero, 25           # Upper case end
+        ble t0, t5, add_ucase_count # Is in boundary of upper case letters
 
         addi t0, t4, -97
-        blt t0, zero, loop_cases # Is not a letter
-        beqz t0, add_lcase_count # Checker Lower case start
-        addi t5, zero, 25 # Lower case end
-        ble t0, t5, add_lcase_count
+        blt t0, zero, loop_cases    # Is not a letter
+        beqz t0, add_lcase_count    # Checker Lower case start
+        addi t5, zero, 25           # Lower case end
+        ble t0, t5, add_lcase_count # Is in boundary of lower case letters
 
         bnez t4, loop_cases
 
-    print_cases:
-        # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, ucase_msg                  # Buffer address
-        li a2, 12
-        li a7, 64                  # Syscall number for write
-        ecall
-
-        # Get 100s 10ths and 1s
-        li t6, 100
-        div t3, t2, t6 # Hundreds count
-        rem t4, t2, t6 # 10's
-
-        li t6, 10
-        div t1, t4, t6 # 10's count
-        rem t5, t4, t6 # 1's count
-        
-        # Get num ascii code
-        addi t3, t3, 48 # Hundreds
-        addi t1, t1, 48 # Tens
-        addi t5, t5, 48 # Ones
-
-        # Add numbers to stack
-        mv t6, sp
-        sw t3, 0(t6)
-        addi t6, t6, 1
-        sw t1, 0(t6)
-        addi t6, t6, 1
-        sw t5, 0(t6)
-        
-        li a0, 1                   # Stdout file descriptor
-        mv a1, sp               # Buffer address
-        li a2, 10
-        li a7, 64                  # Syscall number for write
-        ecall
-
-        # # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, newline        # Message address
-        li a2, 1                  # Message length
-        li a7, 64                  # Syscall number for write
-        ecall
-        
-        # Print lower case count
-        
-        li a0, 1                   # Stdout file descriptor
-        la a1, lcase_msg                  # Buffer address
-        li a2, 12
-        li a7, 64                  # Syscall number for write
-        ecall
-        
-        mv t2, t3
-
-        # Get 100s 10ths and 1s
-        li t6, 100
-        div t3, t2, t6 # Hundreds count
-        rem t4, t2, t6 # 10's
-
-        li t6, 10
-        div t1, t4, t6 # 10's count
-        rem t5, t4, t6 # 1's count
-        
-        # Get num ascii code
-        addi t3, t3, 48 # Hundreds
-        addi t1, t1, 48 # Tens
-        addi t5, t5, 48 # Ones
-
-        # Add numbers to stack
-        mv t6, sp
-        sw t3, 0(t6)
-        addi t6, t6, 1
-        sw t1, 0(t6)
-        addi t6, t6, 1
-        sw t5, 0(t6)
-        
-        li a0, 1                   # Stdout file descriptor
-        mv a1, sp               # Buffer address
-        li a2, 10
-        li a7, 64                  # Syscall number for write
-        ecall
-
-        # # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, newline        # Message address
-        li a2, 1                  # Message length
-        li a7, 64                  # Syscall number for write
-        ecall
-
+    go_back:
+        jalr x0, x1, 0
 
     jalr x0, x1, 0
 
@@ -212,51 +230,7 @@ count_words:
         beq t4, t5, add_word_count
 
         bnez t4, loop_words
-
-    print_words:
-        # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, word_msg                  # Buffer address
-        li a2, 8
-        li a7, 64                  # Syscall number for write
-        ecall
-
-        # Get 100s 10ths and 1s
-        li t6, 100
-        div t3, t2, t6 # Hundreds count
-        rem t4, t2, t6 # 10's
-
-        li t6, 10
-        div t1, t4, t6 # 10's count
-        rem t5, t4, t6 # 1's count
         
-        # Get num ascii code
-        addi t3, t3, 48 # Hundreds
-        addi t1, t1, 48 # Tens
-        addi t5, t5, 48 # Ones
-
-        # Add numbers to stack
-        mv t6, sp
-        sw t3, 0(t6)
-        addi t6, t6, 1
-        sw t1, 0(t6)
-        addi t6, t6, 1
-        sw t5, 0(t6)
-        
-        # Write stack to stdout
-        li a0, 1                   # Stdout file descriptor
-        mv a1, sp               # Buffer address
-        li a2, 10
-        li a7, 64                  # Syscall number for write
-        ecall
-        
-        # # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, newline        # Message address
-        li a2, 1                  # Message length
-        li a7, 64                  # Syscall number for write
-        ecall
-
     jalr x0, x1, 0
 
 
@@ -282,49 +256,7 @@ count_sentences:
 
         bnez t4, loop_sentence
 
-    print_sentence:
-        # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, sentence_msg        # Message address
-        li a2, 12                  # Message length
-        li a7, 64                  # Syscall number for write
-        ecall
 
-        # Get 100s 10ths and 1s
-        li t6, 100
-        div t3, t2, t6 # Hundreds count
-        rem t4, t2, t6 # 10's
-
-        li t6, 10
-        div t1, t4, t6 # 10's count
-        rem t5, t4, t6 # 1's count
-        
-        # Get num ascii code
-        addi t3, t3, 48 # Hundreds
-        addi t1, t1, 48 # Tens
-        addi t5, t5, 48 # Ones
-
-        # Add numbers to stack
-        mv t6, sp
-        sw t3, 0(t6)
-        addi t6, t6, 1
-        sw t1, 0(t6)
-        addi t6, t6, 1
-        sw t5, 0(t6)
-        
-        # Write stack to stdout
-        li a0, 1                   # Stdout file descriptor
-        mv a1, sp               # Buffer address
-        li a2, 10
-        li a7, 64                  # Syscall number for write
-        ecall
-        
-        # # Write to stdout
-        li a0, 1                   # Stdout file descriptor
-        la a1, newline        # Message address
-        li a2, 1                  # Message length
-        li a7, 64                  # Syscall number for write
-        ecall
 
     jalr x0, x1, 0
 
